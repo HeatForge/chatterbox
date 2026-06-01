6f026f7a31fa7f5de7613f40fc8e7a303169afb6
HeatForge <heattheforge@gmail.com>
2026-06-01 21:13:23 +0200

    Fixes

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,3 +1,5 @@
+import "server-only";
+
 import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
@@ -9,9 +11,6 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 export const appRouter = createTRPCRouter({
   post: postRouter,
 });
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
 
 /**
  * Create a server-side caller for the tRPC API.
